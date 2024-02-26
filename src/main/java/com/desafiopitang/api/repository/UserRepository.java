f821d3a65ebd48844b9d747af88ac066bd77621e
drrqueiroz <drrqueiroz@gmail.com>
2024-02-26 16:34:55 -0300

    Cadastrar Usuário - inclusão do decorater @Repository

--- src/main/java/com/desafiopitang/api/repository/UserRepository.java
+++ src/main/java/com/desafiopitang/api/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.desafiopitang.api.repository;
 
 import com.desafiopitang.api.domain.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository {
 
     Optional<User> findUserByLogin(String login);
