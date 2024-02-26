69d73ad48e7e2b8273e64893668eb619d0cd8128
drrqueiroz <drrqueiroz@gmail.com>
2024-02-26 16:46:59 -0300

    Cadastrar Usuário - atualização

--- src/main/java/com/desafiopitang/api/repository/UserRepository.java
+++ src/main/java/com/desafiopitang/api/repository/UserRepository.java
@@ -1,14 +1,16 @@
 package com.desafiopitang.api.repository;
 
 import com.desafiopitang.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByLogin(String login);
 
     Optional<User> findUserByEmail(String email);
+
 }
