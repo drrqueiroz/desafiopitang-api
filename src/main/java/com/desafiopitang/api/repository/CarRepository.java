ec58a0f52c77a903f0305eca1ef5176524553594
drrqueiroz <drrqueiroz@gmail.com>
2024-02-26 16:38:23 -0300

    Cadastrar Carro - inclusão do decorater @Repository

--- src/main/java/com/desafiopitang/api/repository/CarRepository.java
+++ src/main/java/com/desafiopitang/api/repository/CarRepository.java
@@ -3,10 +3,12 @@ package com.desafiopitang.api.repository;
 import com.desafiopitang.api.domain.Car;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CarRepository {
     Optional<Car> findCarByLicensePlate(String licensePlate);
 
