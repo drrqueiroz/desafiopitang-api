7b9f34a921437b6258bdaa1998cc7e9040a3f774
drrqueiroz <drrqueiroz@gmail.com>
2024-02-26 17:28:44 -0300

    Classe de configuração do profile de test

---
+++ src/main/java/com/desafiopitang/api/configuration/TestConfig.java
@@ -0,0 +1,30 @@
+package com.desafiopitang.api.configuration;
+
+import com.desafiopitang.api.domain.User;
+import com.desafiopitang.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private UserRepository repositoryUser;
+
+    @Bean
+    public boolean loadDB() {
+        User user = new User(null, "Davidson", "Queiroz", "drrqueiroz@gmail.com", LocalDate.parse("1990-05-01", DateTimeFormatter.ISO_LOCAL_DATE), "drrqueiroz", "123", "9123456", LocalDate.now(), null);
+        User user2 =new User(null, "David", "Lopes", "david@gmail.com", LocalDate.parse("1990-05-01", DateTimeFormatter.ISO_LOCAL_DATE), "david", "123", "94556255", LocalDate.now(), null);
+
+        repositoryUser.saveAll(List.of(user, user2));
+        return true;
+    }
+
+}
