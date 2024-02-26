7f2f5d85f5544c0aaa0ef46c463a673112534178
drrqueiroz <drrqueiroz@gmail.com>
2024-02-26 16:39:26 -0300

    Cadastrar Usuário - interface UserService

---
+++ src/main/java/com/desafiopitang/api/services/UserService.java
@@ -0,0 +1,48 @@
+package com.desafiopitang.api.services;
+
+import com.desafiopitang.api.domain.User;
+import com.desafiopitang.api.dto.UserDTO;
+import com.desafiopitang.api.dto.UserMeDTO;
+
+import java.util.List;
+
+public interface UserService {
+    /**
+     *
+     * @return
+     */
+    public List<User> findAll();
+
+    /**
+     *
+     * @return
+     */
+    public UserMeDTO findByMe();
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public User findById(long id);
+    /**
+     *
+     * @param userDTO
+     * @return
+     */
+    public User insert(UserDTO userDTO);
+
+    /**
+     *
+     * @param userDTO
+     * @return
+     */
+    public User update(UserDTO userDTO);
+
+    /**
+     *
+     * @param userDTO
+     * @return
+     */
+    public void delete(long id);
+}
