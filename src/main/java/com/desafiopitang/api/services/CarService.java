91a1b9deacca8dabc7be59c88f1df7db1fcb881c
drrqueiroz <drrqueiroz@gmail.com>
2024-02-26 16:39:11 -0300

    Cadastrar Carro - interface CarService

---
+++ src/main/java/com/desafiopitang/api/services/CarService.java
@@ -0,0 +1,58 @@
+package com.desafiopitang.api.services;
+
+import com.desafiopitang.api.domain.Car;
+import com.desafiopitang.api.dto.CarDTO;
+
+import java.util.List;
+
+public interface CarService {
+    /**
+     *
+     * @return
+     */
+    public List<Car> findAll();
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public Car findById(Long id);
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public List<Car> findCarByUserList(Long id);
+
+    /**
+     *
+     * @param carDTO
+     * @return
+     */
+    public Car insert(CarDTO carDTO);
+
+    /**
+     *
+     * @param car
+     * @return
+     */
+    public Car insert(Car car);
+
+    /**
+     *
+     * @param id
+     * @param carDTO
+     * @return
+     */
+    public Car update(Long id, CarDTO carDTO);
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public void delete(Long id);
+
+}
