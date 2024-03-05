d19e3a2aa6c99a05f485aa6485f344b4c8d39dfe
drrqueiroz <drrqueiroz@gmail.com>
2024-03-05 00:29:42 -0300

    Autenticar no Sistema - Implementando a configuracao de cors

---
+++ src/main/java/com/desafiopitang/api/configuration/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.desafiopitang.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
+}
