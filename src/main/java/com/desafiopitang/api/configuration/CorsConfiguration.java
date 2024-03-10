837b86c9f64695ed3989d3e09c31cfd4fef46b62
drrqueiroz <drrqueiroz@gmail.com>
2024-03-10 19:00:55 -0300

    Liberando o dominio de publicado no render.com CorsRegistry

--- src/main/java/com/desafiopitang/api/configuration/CorsConfiguration.java
+++ src/main/java/com/desafiopitang/api/configuration/CorsConfiguration.java
@@ -1,12 +1,8 @@
 package com.desafiopitang.api.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.time.Duration;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
@@ -14,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://desafiopitang-view.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
